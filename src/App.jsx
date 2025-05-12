cd083e718c11cc1a34f5b82762e982f76479d8f9
Julio0885 <julioc0885@gmail.com>
2025-05-12 15:02:19 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar/Navbar'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <Navbar />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar/DarkMode.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react'
+
+
+export default function DarkMode() {
+  const [theme, setTheme] = useState(() => {
+    // obten el tema guardado en localstorage o usa 'light'
+    return localStorage.getItem('theme') || 'light';
+  })
+
+  const toggleTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+    localStorage.setItem('theme', newTheme);
+  };
+
+
+  useEffect(() => {
+    const element = document.documentElement;
+    if (theme === "dark") {
+      element.classList.add("dark");
+    } else {
+      element.classList.remove("dark")
+    }
+  }, [theme]);
+
+  return (
+    <div>
+      <img 
+      className="w-12 cursor-pointer"  
+      src={theme === 'light' ? "/assets/website/light-mode-button.png" : "/assets/website/dark-mode-button.png"}
+      alt="Toggle Theme"
+      onClick={toggleTheme}/>
+    </div>
+  )
+}
+
+
